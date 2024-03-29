c63eed3b5d0c6dfda0e4cfb63ca38eeaa6af648c
GHASSEN MRAD <135726295+MradGhassen@users.noreply.github.com>
2024-03-30 00:08:42 +0200

    Add files via upload

---
+++ components/ImageScrollbar.jsx
@@ -0,0 +1,54 @@
+import { useContext } from 'react';
+import Image from 'next/image';
+import { Box, Flex } from '@chakra-ui/react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
+import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
+
+const LeftArrow = () => {
+  const { scrollPrev } = useContext(VisibilityContext);
+
+  return (
+    <Flex justifyContent='center' alignItems='center' marginRight='1'>
+      <FaArrowAltCircleLeft
+        onClick={() => scrollPrev()}
+        fontSize='2xl'
+        cursor='pointer'
+        d={['none','none','none','block']}
+      />
+    </Flex>
+  );
+}
+
+const RightArrow = () => {
+  const { scrollNext } = useContext(VisibilityContext);
+
+  return (
+    <Flex justifyContent='center' alignItems='center' marginLeft='1'>
+      <FaArrowAltCircleRight
+        onClick={() => scrollNext()}
+        fontSize='2xl'
+        cursor='pointer'
+        d={['none','none','none','block']}
+    />
+    </Flex>
+  );
+}
+
+export default function ImageSrollbar({ data }) {
+  return (
+    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{ overflow: 'hidden' }} >
+      {data.map((item) => (
+        <Box key={item.id} width={['100%', '50%', '33.33%', '25%']} p='1'>
+          <Image
+            placeholder="blur"
+            blurDataURL={item.url}
+            src={item.url}
+            width={1000}
+            height={500}
+            sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
+          />
+        </Box>
+      ))}
+    </ScrollMenu>
+  );
+}
